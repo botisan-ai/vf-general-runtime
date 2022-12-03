bffeff3659301c6149b6a4a5b3e984ccc789ff42
lanz0519 <1035305632@qq.com>
2022-12-03 18:09:28 +0800

    feat(blog): local api work with multiple vf.files

--- tests/runtime/lib/DataAPI/localDataAPI.unit.ts
+++ tests/runtime/lib/DataAPI/localDataAPI.unit.ts
@@ -54,7 +54,7 @@ describe('localDataAPI client unit tests', () => {
 
   it('getVersion', async () => {
     const content = {
-      version: 'version-val',
+      version: { _id: '635fed30c361a59742273aeb' },
       project: 'project-val',
       programs: 'programs-val',
     };
@@ -71,7 +71,7 @@ describe('localDataAPI client unit tests', () => {
       { fs: stubFS as any, path: path as any }
     );
 
-    expect(await LocalDataApi.getVersion()).to.eql(content.version);
+    expect(await LocalDataApi.getVersion('635fed30c361a59742273aeb')).to.eql(content.version);
   });
 
   it('unhashVersionID', async () => {
@@ -123,7 +123,7 @@ describe('localDataAPI client unit tests', () => {
   it('getProject', async () => {
     const content = {
       version: 'version-val',
-      project: 'project-val',
+      project: { _id: '635fed30ac000c0006911e17' },
       programs: 'programs-val',
     };
     const stubFS = {
@@ -139,7 +139,7 @@ describe('localDataAPI client unit tests', () => {
       { fs: stubFS as any, path: path as any }
     );
 
-    expect(await LocalDataApi.getProject()).to.eql(content.project);
+    expect(await LocalDataApi.getProject('635fed30ac000c0006911e17')).to.eql(content.project);
   });
 
   it('fetchDisplayById', async () => {
@@ -162,5 +162,78 @@ describe('localDataAPI client unit tests', () => {
     );
 
     expect(await LocalDataApi.fetchDisplayById()).to.eql(null);
+  });
+
+  it('getProjectNLP', async () => {
+    const content = {
+      version: 'version-val',
+      project: {
+        _id: '635fed30ac000c0006911e17',
+        prototype: {
+          nlp: {
+            type: 'LUIS',
+            appID: '684303ce-6e54-4965-ae13-ab50ede4f409',
+            resourceID: 'alebdgdn:https://westus.api.cognitive.microsoft.com/',
+          },
+        },
+        devVersion: '635fed30c361a59742273aeb',
+        liveVersion: '6370b765c8ff0d00074de4bc',
+        platformData: {
+          invocationName: 'template project general',
+        },
+      },
+      programs: 'programs-val',
+    };
+    const stubFS = {
+      readFileSync: sinon.stub().returns('readFileSync-val'),
+    };
+    const jsonParseStub = sinon.stub(JSON, 'parse').returns(content as any);
+    const path = {
+      join: sinon.stub().returns('join-val'),
+    };
+
+    const LocalDataApi = new LocalDataAPI(
+      { projectSource: 'projectSource-val' },
+      { fs: stubFS as any, path: path as any }
+    );
+
+    expect(await LocalDataApi.getProjectNLP('635fed30ac000c0006911e17')).to.deep.include({
+      nlp: {
+        type: 'LUIS',
+        appID: '684303ce-6e54-4965-ae13-ab50ede4f409',
+        resourceID: 'alebdgdn:https://westus.api.cognitive.microsoft.com/',
+      },
+      devVersion: '635fed30c361a59742273aeb',
+      liveVersion: '6370b765c8ff0d00074de4bc',
+      platformData: {
+        invocationName: 'template project general',
+      },
+    });
+  });
+
+  it('getProjectUsingAPIKey', async () => {
+    const content = {
+      version: 'version-val',
+      project: { _id: '635fed30ac000c0006911e17' },
+      programs: 'programs-val',
+    };
+    const stubFS = {
+      readFileSync: sinon.stub().returns('readFileSync-val'),
+    };
+    const jsonParseStub = sinon.stub(JSON, 'parse').returns(content as any);
+    const path = {
+      join: sinon.stub().returns('join-val'),
+    };
+
+    const LocalDataApi = new LocalDataAPI(
+      // eslint-disable-next-line no-secrets/no-secrets
+      { projectSource: 'projectSource-val*KEY*635fed4e7a3ae00007cbddad' },
+      { fs: stubFS as any, path: path as any }
+    );
+
+    expect(
+      // eslint-disable-next-line no-secrets/no-secrets
+      await LocalDataApi.getProjectUsingAPIKey('VF.DM.635fed4e7a3ae00007cbddad.27Cl22N4qxipRWm0')
+    ).to.have.property('_id', '635fed30ac000c0006911e17');
   });
 });
