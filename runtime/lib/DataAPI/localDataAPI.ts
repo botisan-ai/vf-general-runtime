3c6cdbd1c9b8dbdf984861d688b1143bf08a8838
lanz0519 <1035305632@qq.com>
2022-12-03 18:09:28 +0800

    feat(blog): local api work with multiple vf.files

--- runtime/lib/DataAPI/localDataAPI.ts
+++ runtime/lib/DataAPI/localDataAPI.ts
@@ -3,6 +3,7 @@ import * as FS from 'fs';
 import * as Path from 'path';
 
 import { DataAPI } from './types';
+import { extractAPIKeyID } from './utils';
 
 class LocalDataAPI<
   P extends BaseModels.Program.Model<any, any> = BaseModels.Program.Model<any, any>,
@@ -10,27 +11,68 @@ class LocalDataAPI<
   PJ extends BaseModels.Project.Model<any, any> = BaseModels.Project.Model<AnyRecord, AnyRecord>
 > implements DataAPI<P, V, PJ>
 {
-  private version: V;
+  private versions: Array<V>;
 
-  private project: PJ;
+  private projects: Array<PJ>;
 
   private programs: Record<string, P>;
+
+  private apiKeyIDs: Record<string, string> = {};
 
   constructor({ projectSource }: { projectSource: string }, { fs, path }: { fs: typeof FS; path: typeof Path }) {
     if (!projectSource) throw new Error('project source undefined');
 
-    const content = JSON.parse(fs.readFileSync(path.join('projects', projectSource), 'utf8'));
+    const projectSources = projectSource.split('|||');
 
-    this.version = content.version;
-    this.project = content.project;
-    this.programs = content.programs;
+    this.versions = [];
+    this.projects = [];
+    this.programs = {};
+
+    projectSources.forEach((projectSourceString: string) => {
+      const projectSource = projectSourceString.split('*KEY*')[0];
+      const content = JSON.parse(fs.readFileSync(path.join('projects', projectSource), 'utf8'));
+
+      if (!this.projects.find((project) => project._id === content.project._id)) {
+        const key = projectSourceString.split('*KEY*')[1];
+        this.apiKeyIDs[key] = content.project._id;
+
+        this.versions.push(content.version);
+        this.projects.push(content.project);
+        Object.assign(this.programs, content.programs);
+      }
+    });
   }
 
-  public getVersion = async () => this.version;
+  public init = async () => {
+    // no-op
+  };
+
+  public getVersion = async (versionID: string) => {
+    return this.versions.find((version) => version._id === versionID) as V;
+  };
 
   public getProgram = async (programID: string) => this.programs[programID];
 
-  public getProject = async () => this.project;
+  public getProject = async (projectID: string) => {
+    return this.projects.find((project) => project._id === projectID) as PJ;
+  };
+
+  public fetchDisplayById = async () => null;
+
+  public getProjectNLP = async (projectID: string) => {
+    const data = this.projects.find((project) => project._id === projectID) as PJ;
+    return {
+      nlp: data.prototype?.nlp,
+      devVersion: data.devVersion,
+      liveVersion: data.liveVersion,
+      platformData: data.platformData,
+    };
+  };
+
+  public getProjectUsingAPIKey = async (key: string) => {
+    const apiKeyID = extractAPIKeyID(key);
+    return this.projects.find((project) => project._id === this.apiKeyIDs[apiKeyID || '']) as PJ;
+  };
 }
 
 export default LocalDataAPI;
